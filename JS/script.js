10f8bc04792fb23bb00520b8fb8ade8f88d5a7c5
eliotinginjesus <88655734+eliotinginjesus@users.noreply.github.com>
2025-06-06 18:29:41 +0700

    responsive layout

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  const slides = document.getElementsByClassName("img-slideshow");
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+// Auto-slide tiap 1 detik
+setInterval(() => {
+  plusDivs(1); // Auto jalan
+}, 1000);
